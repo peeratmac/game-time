bf951cfb535c575dd024cc4c4eed3c41bc340941
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-31 15:31:12 -0600

    Add test for puzzle storage

--- test/Puzzle-test.js
+++ test/Puzzle-test.js
@@ -7,9 +7,13 @@ describe('Puzzle', () => {
   let puzzle;
 
   beforeEach(() => {
-    puzzle = new Puzzle(data);
+    puzzle = new Puzzle(data.puzzles);
   });
-  it('should store all the puzzles from the puzzle bank', function() {
+  it('should store all the puzzles from the puzzle bank', function () {
     expect(puzzle.puzzleBank).to.equal(data);
   });
+
+  it.only('should be able to return a random puzzle from the bank', () => {
+    expect(puzzle.getPuzzle().length).to.equal(1);
+  })
 });
