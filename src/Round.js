181fc6ec289069685e09dfd0b59a50f99225dfbf
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-31 16:32:50 -0600

    Add testing/method for storing current player turn

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,13 @@
-import Round from './Round.js';
-import data from './data/sample-data.js';
+import Game from '../test/Round-test.js';
+// import Puzzle from './Puzzle.js'
+// import data from './data/sample-data.js';
 
 class Round {
-  constructor(data) {}
+  constructor(puzzle, game) {
+    this.puzzle = puzzle;
+    this.playerTurn = game.players[0];
+    this.roundStandings = [];
+  }
 }
 
 export default Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,19 +2,30 @@ import chai from 'chai'
 const expect = chai.expect;
 
 import data from '../src/data/sample-data.js'
+import Game from '../src/Game.js'
 import Puzzle from '../src/Puzzle.js';
 import Round from '../src/Round.js';
 
 describe('Round', () => {
-  let puzzle, round;
+  let game, puzzle, round;
 
   beforeEach(() => {
+    game = new Game([{ id: 1, name: 'Chris' }, {id: 2, name: 'Peerat'}, { id: 3, name: 'Victor' }]);    
     puzzle = new Puzzle(data.puzzles);
     puzzle.choosePuzzleBank();
     puzzle = puzzle.choosePuzzle();
-    round = new Round(puzzle);
+    round = new Round(puzzle, game);
   });
-  it.only('should store the current rounds puzzle', () => {
+  it('should store the current rounds puzzle', () => {
     expect(round.puzzle).to.be.an('object');
-  });
+  })
+
+  it.only('should store the current player\'s turn', () => {
+    expect(round.playerTurn).to.be.an('object');
+  })
+
+  it('should store the current round standings', () => {
+    expect(round.roundStandings).to.be.an('array');
+  })
+  
 });
