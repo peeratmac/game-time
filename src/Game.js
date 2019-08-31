0414389f54a02b482295b70023e96a7050f64e22
Chris Basham <chrisdbasham0317@gmail.com>
2019-08-31 14:56:41 -0600

    Add boiler/methods/testing for game class

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,33 @@
 import Game from './Game.js';
 
 class Game {
-  constructor() {}
+  constructor(players, standings, round, bonusRound) {
+    this.players = players;
+    this.gameStandings = standings;
+    this.currentRound = round;
+    this.bonusRound = bonusRound;
+  }
+
+  startGame() {
+    this.gameStandings = [{ id: 1, money: 0 }, { id: 2, money: 0 }, { id: 3, money: 0 }];
+    this.currentRound = 1;
+  }
+
+  startBonus() {
+    this.bonusRound = true;
+  }
+
+  getWinner() {
+    let sortedPlayers = this.gameStandings.sort((playerA, playerB) => {
+      playerA - playerB
+    })
+    let winnerID = sortedPlayers.shift();
+    return this.players.find(elem => elem.id === winnerID.id)
+  }
+
+  endGame() {
+    return true;
+  }
 }
 
 export default Game;
