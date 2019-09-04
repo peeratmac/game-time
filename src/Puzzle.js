604614f861419d3e269ec7236d9fd05103cdc0a4
VPAbraham <vpa456@gmail.com>
2019-09-03 23:23:54 -0600

    Fix issue with puzzle class not storing newPuzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -4,7 +4,7 @@ class Puzzle {
   constructor(puzzles) {
     this.fullBank = puzzles;
     this.chosenBank = [];
-    //! this.newPuzzle = {}; we may not need this
+    this.newPuzzle = {};
   }
 
   choosePuzzleBank() {
@@ -17,6 +17,7 @@ class Puzzle {
 
   setPuzzle() {
     let index = Math.floor(Math.random() * 24);
+    this.newPuzzle = this.chosenBank[index]
     return this.chosenBank[index];
   }
 
